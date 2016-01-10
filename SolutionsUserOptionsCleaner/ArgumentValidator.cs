e6f50da04442cbe039c24fc868fd493712bc1488
erresen <erresen@users.noreply.github.com>
2016-01-10 21:41:19 +0000

    initial code commit

---
+++ SolutionsUserOptionsCleaner/ArgumentValidator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolutionsUserOptionsCleaner
+{
+    class ArgumentValidator
+    {
+        public ArgumentValidator(IList<string> args)
+        {
+            Args = args ?? new List<string>();
+            Rules = new List<Func<IList<string>, bool>>();
+            Errors = new List<string>();
+        }
+        public IList<string> Args { get; private set; }
+
+        public List<Func<IList<string>, bool>> Rules { get; set; }
+        public List<string> Errors { get; private set; }
+        private Action<string> _errorProcessingAction = (err) => Console.WriteLine(err);
+
+        public bool Valid()
+        {
+            RunRules();
+            return Errors.Count == 0;
+        }
+
+        public void ProcessErrors()
+        {
+            Errors.ForEach(_errorProcessingAction);
+        }
+        
+        public void SetErrorProcessor(Action<string> processingAction)
+        {
+            _errorProcessingAction = processingAction;
+        }
+
+        private void RunRules()
+        {
+            foreach (var rule in Rules)
+            {
+                TryRule(rule);
+            }
+        }
+
+        private void TryRule(Func<IList<string>, bool> rule)
+        {
+            try
+            {
+                if (RuleFails(rule))
+                    AddError();
+            }
+            catch (Exception e)
+            {
+                AddError(e);
+            }
+        }
+
+        private bool RuleFails(Func<IList<string>, bool> rule)
+        {
+            return !rule.Invoke(Args);
+        }
+
+        private void AddError()
+        {
+            Errors.Add("Rule returned false.");
+        }
+
+        private void AddError(Exception e)
+        {
+            Errors.Add($"Rule threw exception: {e.Message}");
+        }
+    }
+}
---
+++ SolutionsUserOptionsCleaner/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionsUserOptionsCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArgumentValidator validator = new ArgumentValidator(args);
+            validator.Rules.Add(x => x.Any());
+            validator.Rules.Add(x => Directory.Exists(x[0]));
+            if(!validator.Valid())
+                foreach (var error in validator.Errors)
+                    Console.WriteLine(error);
+
+
+        }
+    }
+}
